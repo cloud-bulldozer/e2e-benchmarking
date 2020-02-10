09fec9ae6d42ce2bd7f893d99a6b5589f129d0a6
Joe Talerico <jtaleric@redhat.com>
2020-02-10 16:16:12 -0500

    Adding compare script

---
+++ workloads/network-perf/compare.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import operator
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--result",help="Touchstone result yaml file",nargs=1,required=True,
+                   dest="result")
+parser.add_argument("--uuid", help="UUID to determine pass/fail from",nargs=1,required=True,
+                    dest="uuid")
+parser.add_argument("--tests", help="Test to compare",nargs=1,choices=['stream','rr'],required=True,
+                    dest="tests")
+parser.add_argument("--protocol", help="Protocol to compare",nargs=1,choices=['tcp','udp'],required=True,
+                    dest="protocol")
+parser.add_argument("--ci", help="Exit code will be 1/0 based on the number of regressions",nargs=1,required=False,
+                    dest="uuid")
+params = parser.parse_args()
+data = None
+
+if params.result :
+    with open(params.result[0]) as file:
+        try:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+        except:
+            print("Error : Not able to load the result file")
+            exit(1)
+
+if len(data) < 1 :
+    print("YAML file contains no data")
+    exit(1)
+
+main_metric = { 'stream': 'avg(norm_byte)','rr' : 'avg(norm_ltcy)' }
+
+test_type = params.tests[0]
+proto = params.protocol[0]
+
+def compare(data):
+     return (max(data.items(), key=operator.itemgetter(1)))
+
+if test_type in data['test_type.keyword'] :
+    if proto in data['test_type.keyword'][test_type]['protocol'] :
+        for size in data['test_type.keyword'][test_type]['protocol'][
+            proto]['message_size'] :
+            for thread in data['test_type.keyword'][test_type]['protocol'][
+                proto]['message_size'][size]['num_threads'] :
+                result = data['test_type.keyword'][test_type]['protocol'][
+                        proto]['message_size'][size]['num_threads'][thread]
+                comparison = compare(result[main_metric[test_type]])
+                if params.uuid[0] not in comparison :
+                    print("Test Failure : UPerf reports lower throughput for, Test: {}, Protocol {}, Message_size {}, Threads {}".format(
+                        test_type,proto,size,thread))
+                    exit(1)
