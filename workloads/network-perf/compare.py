b94465259f7049554e568a4d549e6b20c2a04002
Mohit Sheth <msheth@redhat.com>
2020-02-11 03:11:23 +0000

    accept tolerance as an arg
    set default tolerance value and display all the failures

--- workloads/network-perf/compare.py
+++ workloads/network-perf/compare.py
@@ -25,6 +25,9 @@ parser.add_argument("--tests", help="Test to compare",nargs=1,choices=['stream',
                     dest="tests")
 parser.add_argument("--protocol", help="Protocol to compare",nargs=1,choices=['tcp','udp'],required=True,
                     dest="protocol")
+parser.add_argument("--tolerance", help="Accepted deviation (+/-) from the reference result",nargs=1,required=False,
+                    dest="tolerance", default="5")
+
 params = parser.parse_args()
 data = None
 
@@ -40,13 +43,38 @@ if len(data) < 1 :
     print("YAML file contains no data")
     exit(1)
 
+def percent_change(value, reference):
+    if reference:
+        return abs(((value-reference)*1.0/reference))*100
+    else:
+        return -1
+
+def compare(data, tolerance=5, latency=False):
+    ref_val = data[params.uuid[0]]
+    current_tuple = [(k,v) for k,v in data.items() if k!=params.uuid[0]][0]
+    percent_diff = percent_change(current_tuple[1], ref_val)
+  
+    if percent_diff == -1:
+        return {current_tuple[0]: current_tuple[1]}
+
+    if current_tuple[1] < ref_val and percent_diff > tolerance:
+        if latency:
+            return {params.uuid[0]: ref_val}
+        return {current_tuple[0]: current_tuple[1]}
+
+    elif current_tuple[1] > ref_val and percent_diff > tolerance:
+        if latency:
+            return {current_tuple[0]: current_tuple[1]}
+        return {params.uuid[0]: ref_val}
+
+    else:
+        return {params.uuid[0]: ref_val}
+
 main_metric = { 'stream': 'avg(norm_byte)','rr' : 'avg(norm_ltcy)' }
 
+failed = 0
 test_type = params.tests[0]
 proto = params.protocol[0]
-
-def compare(data):
-     return (max(data.items(), key=operator.itemgetter(1)))
 
 if test_type in data['test_type.keyword'] :
     if proto in data['test_type.keyword'][test_type]['protocol'] :
@@ -56,8 +84,14 @@ if test_type in data['test_type.keyword'] :
                 proto]['message_size'][size]['num_threads'] :
                 result = data['test_type.keyword'][test_type]['protocol'][
                         proto]['message_size'][size]['num_threads'][thread]
-                comparison = compare(result[main_metric[test_type]])
+                if test_type == "rr" :
+                    comparison = compare(result[main_metric[test_type]], int(params.tolerance[0]), latency=True)
+                else:
+                    comparison = compare(result[main_metric[test_type]], tolerance=int(params.tolerance[0]))
                 if params.uuid[0] not in comparison :
-                    print("Test Failure : UPerf reports lower throughput for, Test: {}, Protocol {}, Message_size {}, Threads {}".format(
+                    print("TEST FAILURE: Uperf reports regrerssion which is beyond the acceptable deviation of {}%".format(params.tolerance[0]))
+                    print("Test: {}    Protocol: {}    Message_size: {}    Threads: {}".format(
                         test_type,proto,size,thread))
-                    exit(1)
+                    failed = 1   
+exit(failed)
+
