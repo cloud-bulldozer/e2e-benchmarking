b763e0680bd138ea55b92b8fe1b7db0bbe470d5d
Joe Talerico <jtaleric@redhat.com>
2020-02-13 06:45:53 -0500

    Script to run through compare of results

---
+++ workloads/network-perf/run_network_compare.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -x
+
+datasource="elasticsearch"
+tool="uperf"
+function="compare"
+base_uuid=$1
+if [ "$base_uuid" == "" ]; then
+  echo "Unable to execute compare - no base UUID passed"
+  exit 1
+fi
+compare_uuid=$2
+if [ "$compare_uuid" == "" ]; then
+  echo "Unable to execute compare - no compare UUID passed"
+  exit 1
+fi
+es=${ES_SERVER}
+if [ "$es" == "" ]; then
+  echo "Unable to execute compare - no ES passed"
+  exit 1
+fi
+
+git clone https://github.com/cloud-bulldozer/touchstone
+cd touchstone
+python -m venv ./compare
+source ./compare/bin/activate
+pip install pyyaml
+python3 setup.py develop
+if [[ $? -ne 0 ]] ; then
+  echo "Unable to execute compare - Failed to install touchstone"
+  exit 1
+fi
+
+touchstone_compare $tool $datasource ripsaw -url $es -u $base_uuid $compare_uuid -o yaml | tee ../compare.yaml
+if [[ $? -ne 0 ]] ; then
+  echo "Unable to execute compare - Failed to run touchstone"
+  exit 1
+fi
+
+failed=0
+cd ../
+echo "Checking Stream TCP"
+python compare.py --result compare.yaml --uuid $base_uuid --test stream --protocol tcp || failed=1
+echo "Checking Stream UDP"
+python compare.py --result compare.yaml --uuid $base_uuid --test stream --protocol udp || failed=1
+echo "Checking RR TCP"
+python compare.py --result compare.yaml --uuid $base_uuid --test rr --protocol tcp || failed=1
+echo "Checking RR UDP"
+python compare.py --result compare.yaml --uuid $base_uuid --test rr --protocol tcp || failed=1
+echo "Compare complete"
+exit $failed
