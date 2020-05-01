3757537d885ca9ea3a868fabdd6d18ffd4587ad8
dry923 <rzaleski@redhat.com>
2020-05-01 16:09:32 -0400

    Fixing metadata variable from sa to serviceaccount

--- workloads/network-perf/run_pod_network_test_fromgit.sh
+++ workloads/network-perf/run_pod_network_test_fromgit.sh
@@ -39,7 +39,7 @@ spec:
   test_user: ${cloud_name}-default-ci-${pairs}p
   metadata:
     collection: ${_metadata_collection}
-    sa: backpack-view
+    serviceaccount: backpack-view
     privileged: true
   workload:
     name: uperf
