ecf10b55e35f922d5b49400915a597e1d8bb4423
amitsagtani97 <asagtani06@gmail.com>
2021-01-27 22:07:43 +0530

    added snappy script

---
+++ workloads/run_snappy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+export LC_ALL=en_US.utf-8
+export LANG=en_US.utf-8
+python3 -m pip install --user pipx
+export PATH="${PATH}:$(python3 -c 'import site; print(site.USER_BASE)')/bin"
+pipx install git+https://github.com/openshift-scale/data-server-cli.git
+snappy install
+
+if [[ $? -ne 0 ]] ; then
+  echo "Unable to backup data... Failed to install snappy!"
+  exit 1
+fi
+
+export DATA_SERVER_URL=http://ec2-35-165-90-5.us-west-2.compute.amazonaws.com:7070
+export DATA_SERVER_USERNAME=amit@redhat.com
+export DATA_SERVER_PASSWORD=amit
+
+export file_path=$1
+export SNAPPY_FILE_DIR=$2
+
+set -x
+snappy script-login
+snappy post-file $file_path
+set +x
+
+if [[ $? -ne 0 ]] ; then
+  echo "Unable to backup data - Failed to run Snappy!"
+  exit 1
+fi
+
+deactivate
+rm -rf venv
