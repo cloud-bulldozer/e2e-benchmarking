0376a98262b8358b537690bb75dcc2da454e7008
Harshith u <44830597+Harshith-umesh@users.noreply.github.com>
2021-03-05 17:06:34 +0530

    added a node to scale option , default worker (#135)
    * Update README.md
    
    * Update run_scale_fromgit.sh
    
    * Update README.md
    
    * Update common.sh
    
    * Update run_scale_fromgit.sh

--- workloads/scale-perf/run_scale_fromgit.sh
+++ workloads/scale-perf/run_scale_fromgit.sh
@@ -44,7 +44,7 @@ spec:
     name: scale_openshift
     args:
       label:
-        key: node-role.kubernetes.io/workload
+        key: node-role.kubernetes.io/${_workload_node_role}
         value: ""
       tolerations:
         key: role
