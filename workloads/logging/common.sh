773a44adf5ba7d19a056dc5326b3efae3cdf4d8c
Keith Whitley <kwhitley@redhat.com>
2021-05-11 11:27:32 -0400

    hotfix logging status check

--- workloads/logging/common.sh
+++ workloads/logging/common.sh
@@ -157,7 +157,7 @@ wait_for_benchmark() {
   done
   log "Waiting for log-generator pods to start"
   suuid=$(oc get benchmark -n my-ripsaw log-generator -o jsonpath="{.status.suuid}")
-  until [[ $(oc get pod -n my-ripsaw -l job-name=log-generator-${suuid} --ignore-not-found -o jsonpath="{.items[*].status.phase}" | grep Running | wc -l) -eq $POD_COUNT ]]; do
+  until [[ $(oc get pod -n my-ripsaw -l job-name=log-generator-${suuid} --ignore-not-found -o jsonpath="{.items[*].status.phase}" | grep Running | wc -w) -eq $POD_COUNT ]]; do
     sleep 1
     if [[ $(date +%s) -gt ${timeout} ]]; then
       log "Timeout waiting for all pods to be running"
@@ -165,7 +165,7 @@ wait_for_benchmark() {
     fi
   done
   log "Benchmark in progress"
-  until [[ $(oc get benchmark -n my-ripsaw log-generator -o jsonpath="{.status.state}" | grep -E "Complete|Failed" | wc -l) -eq $POD_COUNT ]]; do
+  until [[ $(oc get benchmark -n my-ripsaw log-generator -o jsonpath="{.status.state}" | grep -E "Complete|Failed" | wc -l) -eq "1" ]]; do
     if [[ $(date +%s) -gt ${timeout} ]]; then
       log "Timeout waiting for Benchmark to complete"
       exit 1
