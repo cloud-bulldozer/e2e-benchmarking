22acf26ea8a033610d5b0d57fb196ecfb8db6277
Raul Sevilla <rsevilla@redhat.com>
2021-08-16 11:41:20 +0200

    Bump kube-burner image
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- workloads/kube-burner/env.sh
+++ workloads/kube-burner/env.sh
@@ -29,7 +29,7 @@ export REMOTE_METRIC_PROFILE=${REMOTE_METRIC_PROFILE}
 export REMOTE_ALERT_PROFILE=${REMOTE_ALERT_PROFILE}
 
 # Kube-burner job
-export KUBE_BURNER_IMAGE=${KUBE_BURNER_IMAGE:-quay.io/cloud-bulldozer/kube-burner:v0.12}
+export KUBE_BURNER_IMAGE=${KUBE_BURNER_IMAGE:-quay.io/cloud-bulldozer/kube-burner:v0.13}
 export NODE_SELECTOR=${NODE_SELECTOR:-'{node-role.kubernetes.io/worker: }'}
 export JOB_TIMEOUT=${JOB_TIMEOUT:-14400}
 export LOG_STREAMING=${LOG_STREAMING:-true}
