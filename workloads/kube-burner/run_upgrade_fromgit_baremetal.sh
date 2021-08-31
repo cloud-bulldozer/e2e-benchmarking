7af2068f60d764b3426e54d6ce8051424f0197d5
jdowni000 <55856631+jdowni000@users.noreply.github.com>
2021-08-31 15:51:40 -0500

    Improve update (#7)
    * first commit: adding calculations for allocatable CPU per MCP
    
    * Changing approach to creating new n number mcps depending on node count
    
    * removing old machineConfig_pool func
    
    * removing bash script testing code left in common.sh
    
    * fixed a couple naming convention issues
    
    * small fixes from testing in actual cluster
    
    * fixed applying mcp.yaml to use envsubst
    
    * more issues resolved from testing
    
    * more fixes in while loops while testing
    
    * completed deployment fix
    
    * changing function name, adding check for mcps and ns if exists
    
    * adding json creation and use of mb
    
    * adding logic to allow MCP_SIZE and MCP_NODE_COUNT to be set as a variable
    
    * changing env var MCP_SIZE to TOTAL_MCPS
    
    * fixed spacing in all of new function
    
    * adding mb_pod.yml and sending mb operations to pod instead of cli
    
    * removing response.csv file as it may be too large depending on how many mcps are genereated
    
    * added var to set to choose whether MCPs are created or not
    
    * just fix...its Friday...its fixed
    
    * fixed if statemnet with elif
    
    * creating logic to check env var inputs
    
    * adding if condition for sleep depending on size of node_count
    
    * adding resources to mb-pod
    
    * adding logic to check for sample app pods to be ready before mb-pod deployment
    
    * Update common.sh
    
    mb-pod and sample app

---
+++ workloads/kube-burner/run_upgrade_fromgit_baremetal.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+. common.sh
+
+export TOTAL_MCPS=${TOTAL_MCPS:-}   # will skip if CREATE_MCPS_BOOL is set to false!
+export MCP_NODE_COUNT=${MCP_NODE_COUNT:-}   # will skip if CREATE_MCPS_BOOL is set to false!
+export CREATE_MCPS_BOOL=true   # true or false
+
+baremetal_upgrade_auxiliary
+exit 1
+
+
+
