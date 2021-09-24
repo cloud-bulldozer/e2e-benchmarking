89b8eefec4f72fdd1d753d370d0f77b79a0bb806
Harshith-umesh <harshith.umesh.nat@gmail.com>
2021-09-24 08:34:01 +0000

    new pod-density-heavy workload

---
+++ workloads/kube-burner/run_poddensity-heavy_test_fromgit.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash -e
+
+set -e
+
+export WORKLOAD=pod-density
+export TEST_JOB_ITERATIONS=${PODS:-1000}
+export REMOTE_CONFIG=${REMOTE_CONFIG:-https://raw.githubusercontent.com/Harshith-umesh/e2e-benchmarking/master/workloads/kube-burner/workloads/node-pod-density/pod-density-heavy.yml}
+export REMOTE_METRIC_PROFILE=${REMOTE_METRIC_PROFILE:-https://raw.githubusercontent.com/cloud-bulldozer/e2e-benchmarking/master/workloads/kube-burner/metrics-profiles/metrics.yml}
+
+. common.sh
+
+deploy_operator
+check_running_benchmarks
+deploy_workload
+wait_for_benchmark ${WORKLOAD}
+rm -rf benchmark-operator
+if [[ ${CLEANUP_WHEN_FINISH} == "true" ]]; then
+  cleanup
+fi
+exit ${rc}
