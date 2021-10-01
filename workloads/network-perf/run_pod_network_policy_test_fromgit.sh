f83ee2780d1c4d62282f865da0ef21be19e27736
Raul Sevilla <rsevilla@redhat.com>
2021-10-01 12:48:49 +0200

    Benchmark-operator handler: network-perf
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- workloads/network-perf/run_pod_network_policy_test_fromgit.sh
+++ workloads/network-perf/run_pod_network_policy_test_fromgit.sh
@@ -4,14 +4,11 @@ export NETWORK_POLICY=true
 
 source ./common.sh
 
-for pairs in 1 2 4
-do
-export pairs=${pairs}
-deploy_workload
-wait_for_benchmark
-assign_uuid
-run_benchmark_comparison
-delete_benchmark
+for pairs in 1 2 4; do
+  export pairs=${pairs}
+  deploy_workload
+  assign_uuid
+  run_benchmark_comparison
+  delete_benchmark
 done
-print_uuid
 generate_csv
--- workloads/network-perf/smoke_test.sh
+++ workloads/network-perf/smoke_test.sh
@@ -3,16 +3,13 @@ export WORKLOAD=pod
 export CRD=smoke-crd.yaml
 source ./common.sh
 
-for pairs in 1 2 4
-do
-export pairs=${pairs}
-deploy_workload
-wait_for_benchmark
-assign_uuid
-run_benchmark_comparison
-delete_benchmark
+for pairs in 1 2 4; do
+  export pairs=${pairs}
+  deploy_workload
+  assign_uuid
+  run_benchmark_comparison
+  delete_benchmark
 done
-print_uuid
 generate_csv
 
 if [[ ${ENABLE_SNAPPY_BACKUP} == "true" ]] ; then
