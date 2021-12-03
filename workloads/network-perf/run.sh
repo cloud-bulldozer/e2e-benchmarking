255516de2d1bf62fdd8273cf1958621ddc5d356b
Keith Whitley <kwhitley@redhat.com>
2021-12-03 09:49:52 -0500

    adding script that runs any generic uperf run

---
+++ workloads/network-perf/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source ./common.sh
+export WORKLOAD=${WORKLOAD}
+
+  if [[ $WORKLOAD == "hostnet" ]]; then
+    export HOSTNETWORK=true
+  elif [[ $WORKLOAD == "" ]]; then
+    export SERVICEIP=true 
+  fi
+
+export pairs=${PAIRS:-1}
+export UUID=${UUID:-$(uuidgen)}
+
+run_workload ripsaw-uperf-crd.yaml
+if [[ $? != 0 ]]; then
+  exit 1
+fi
+
+log "Finished workload ${0}"
