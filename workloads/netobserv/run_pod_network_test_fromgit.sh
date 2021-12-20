e79d879e1350bc6b006a38a60654fbfdb976634a
memodi <memodi@redhat.com>
2021-12-20 17:12:55 -0500

    adding netobserv functionalities

--- workloads/netobserv/run_pod_network_test_fromgit.sh
+++ workloads/netobserv/run_pod_network_test_fromgit.sh
@@ -6,13 +6,16 @@ export WORKLOAD=pod
 for pairs in 1 2 4; do
   export UUID=$(uuidgen)
   export pairs
+  deploy_netobserv_operator
+  run_workload ripsaw-uperf-crd.yaml
+  delete_flowcollector
   run_workload ripsaw-uperf-crd.yaml
   if [[ $? != 0 ]]; then
     exit 1
   fi
   BASELINE_UUID=${BASELINE_POD_UUID[${i}]}
-  COMPARISON_OUTPUT="${PWD}/pod-${pairs}-pairs.csv"
-  run_benchmark_comparison
+  # COMPARISON_OUTPUT="${PWD}/pod-${pairs}-pairs.csv"
+  # run_benchmark_comparison
 done
 generate_csv
 
