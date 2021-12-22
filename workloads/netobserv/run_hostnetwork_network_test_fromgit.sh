521225e918191a843b23f07b94e7d7992be3ee0e
memodi <memodi@redhat.com>
2021-12-22 11:17:37 -0500

    simplified code

--- workloads/netobserv/run_hostnetwork_network_test_fromgit.sh
+++ workloads/netobserv/run_hostnetwork_network_test_fromgit.sh
@@ -4,21 +4,15 @@ source ./common.sh
 export WORKLOAD=hostnet
 export HOSTNETWORK=true
 export pairs=1
-export UUID=$(uuidgen)
 
-deploy_netobserv_operator
-run_workload ripsaw-uperf-crd.yaml
+BASELINE_UUID=${BASELINE_HOSTNET_UUID}
+COMPARISON_OUTPUT="${PWD}/hostnet-w-netobserv.csv"
+run_perf_test_w_netobserv
+COMPARISON_OUTPUT="${PWD}/hostnet-wo-netobserv.csv"
+run_perf_test_wo_netobserv
 if [[ $? != 0 ]]; then
   exit 1
 fi
-
-delete_flowcollector
-export UUID=$(uuidgen)
-run_workload ripsaw-uperf-crd.yaml
-
-BASELINE_UUID=${BASELINE_HOSTNET_UUID}
-COMPARISON_OUTPUT="${PWD}/hostnet.csv"
-run_benchmark_comparison
 
 if [[ ${ENABLE_SNAPPY_BACKUP} == "true" ]] ; then
   snappy_backup network_perf_hostnetwork_test
