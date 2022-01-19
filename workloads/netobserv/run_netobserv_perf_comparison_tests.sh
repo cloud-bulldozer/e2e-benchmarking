17d463c91d4297d1970a24aedcf3c4a70f3b794c
memodi <memodi@redhat.com>
2022-01-19 12:01:12 -0500

    use custom uperf

--- workloads/netobserv/run_netobserv_perf_comparison_tests.sh
+++ workloads/netobserv/run_netobserv_perf_comparison_tests.sh
@@ -15,15 +15,15 @@ run_perf_test_w_netobserv
 if [[ $NETWORK_VARIANT == "POD_NETWORK" ]]; then
     cd ../network-perf && ./run_pod_network_test_fromgit.sh $RIPSAW_UPERF_PATH
     run_perf_test_wo_netobserv
-    cd ../network-perf && ./run_pod_network_test_fromgit.sh
+    cd ../network-perf && ./run_pod_network_test_fromgit.sh $RIPSAW_UPERF_PATH
 elif [[ $NETWORK_VARIANT == "SERVICEIP_NETWORK" ]]; then
-    cd ../network-perf && ./run_serviceip_network_test_fromgit.sh
+    cd ../network-perf && ./run_serviceip_network_test_fromgit.sh $RIPSAW_UPERF_PATH
     run_perf_test_wo_netobserv
-    cd ../network-perf && ./run_serviceip_network_test_fromgit.sh
+    cd ../network-perf && ./run_serviceip_network_test_fromgit.sh $RIPSAW_UPERF_PATH
 elif [[ $NETWORK_VARIANT == "MULTUS_NETWORK" ]]; then
-    cd ../network-perf && run_multus_network_tests_fromgit.sh
+    cd ../network-perf && run_multus_network_tests_fromgit.sh $RIPSAW_UPERF_PATH
     run_perf_test_wo_netobserv
-    cd ../network-perf && run_multus_network_tests_fromgit.sh
+    cd ../network-perf && run_multus_network_tests_fromgit.sh $RIPSAW_UPERF_PATH
 else
     echo "unsupported network variant for network-observability"
     exit 1
