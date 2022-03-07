9c2c0c21f995560908c91844367e8039b38a217a
amitsagtani97 <asagtani@redhat.com>
2022-03-07 17:58:33 +0530

    add entry function

--- workloads/cluster-install/run_installtiming_test_fromgit.sh
+++ workloads/cluster-install/run_installtiming_test_fromgit.sh
@@ -3,21 +3,20 @@
 set -o pipefail
 set -eu
 
-init_vars() {
-    export NO_OF_INSTALLS="${NO_OF_INSTALLS:=5}"
-    export PLATFORM="${PLATFORM}"
 
-    if [ $PLATFORM == 'aws' ]
-    then
-        source trigger_scale_ci_deploy.sh $PLATFORM $NO_OF_INSTALLS
-    elif [ $PLATFORM == 'gcp' ]
-    then
-        source trigger_scale_ci_deploy.sh $PLATFORM $NO_OF_INSTALLS
-    if [ $PLATFORM == 'azure' ]
-    then
-        source trigger_scale_ci_deploy.sh $PLATFORM $NO_OF_INSTALLS
-    fi
-}
+export NO_OF_INSTALLS="${NO_OF_INSTALLS:=5}"
+export PLATFORM="${PLATFORM}"
+
+if [ $PLATFORM == 'aws' ]
+then
+    source trigger_scale_ci_deploy.sh $PLATFORM $NO_OF_INSTALLS
+elif [ $PLATFORM == 'gcp' ]
+then
+    source trigger_scale_ci_deploy.sh $PLATFORM $NO_OF_INSTALLS
+if [ $PLATFORM == 'azure' ]
+then
+    source trigger_scale_ci_deploy.sh $PLATFORM $NO_OF_INSTALLS
+fi
 
 
 
