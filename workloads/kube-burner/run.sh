25791676e82cd866a083b93cfb95cb926e329662
Raul Sevilla <rsevilla@redhat.com>
2022-03-23 12:41:41 +0100

    Enable alerting for cluster-density
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- workloads/kube-burner/run.sh
+++ workloads/kube-burner/run.sh
@@ -10,6 +10,7 @@ case ${WORKLOAD} in
   cluster-density)
     WORKLOAD_TEMPLATE=workloads/cluster-density/cluster-density.yml
     METRICS_PROFILE=${METRICS_PROFILE:-metrics-profiles/metrics-aggregated.yaml}
+    ALERTS_PROFILE=${ALERTS_PROFILE:-alert-profiles/generalistic.yml}
     export TEST_JOB_ITERATIONS=${JOB_ITERATIONS:-1000}
   ;;
   node-density)
