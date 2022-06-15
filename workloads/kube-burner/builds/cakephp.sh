c887d3f61f165ff8bfcd172f9e36df8ccd935737
Paige Rubendall <prubenda@redhat.com>
2022-06-15 12:02:04 -0400

    adding benchmark helper

--- workloads/kube-burner/builds/cakephp.sh
+++ workloads/kube-burner/builds/cakephp.sh
@@ -3,7 +3,7 @@ export APP=cakephp
 export BUILD_IMAGE_STREAM=cakephp-mysql-example
 
 export SOURCE_STRAT_ENV=COMPOSER_MIRROR
-export SOURCE_STRAT_FROM_VERSION=latest
+export SOURCE_STRAT_FROM_VERSION=7.4-ubi8
 
 export SOURCE_STRAT_FROM=php
 export POST_COMMIT_SCRIPT=./vendor/bin/phpunit
