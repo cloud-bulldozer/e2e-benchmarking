85be58772d7a0e9172d8b46a64705f51ccd04b60
Murali Krishnasamy <mukrishn@redhat.com>
2022-07-28 08:45:20 -0400

    read local env var

--- workloads/hyper-scale/env.sh
+++ workloads/hyper-scale/env.sh
@@ -2,28 +2,28 @@
 
 ## ROSA input vars
 export AWS_REGION=${AWS_REGION:-us-west-2}
-export AWS_ACCESS_KEY_ID=""
-export AWS_SECRET_ACCESS_KEY=""
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-""}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-""}
 export ROSA_ENVIRONMENT=${ROSA_ENVIRONMENT:-staging}
-export ROSA_TOKEN=""
-export PULL_SECRET=""
+export ROSA_TOKEN=${ROSA_TOKEN:-""}
+export PULL_SECRET=${PULL_SECRET:-""}
 
 # Hosted clusters 
 export NUMBER_OF_HOSTED_CLUSTER=${NUMBER_OF_HOSTED_CLUSTER:-2}
 export COMPUTE_WORKERS_NUMBER=${COMPUTE_WORKERS_NUMBER:-24}
 
 # Hosted cluster spec
-export HYPERSHIFT_OPERATOR_IMAGE="quay.io/hypershift/hypershift-operator:latest"
-export RELEASE_IMAGE=""
-export CPO_IMAGE=""
+export HYPERSHIFT_OPERATOR_IMAGE=${HYPERSHIFT_OPERATOR_IMAGE:-"quay.io/hypershift/hypershift-operator:latest"}
+export RELEASE_IMAGE=${RELEASE_IMAGE:-""}
+export CPO_IMAGE=${CPO_IMAGE:-""}
 export NETWORK_TYPE=${NETWORK_TYPE:-OpenShiftSDN}
 export REPLICA_TYPE=${REPLICA_TYPE:-HighlyAvailable}
 export COMPUTE_WORKERS_TYPE=${COMPUTE_WORKERS_TYPE:-m5.4xlarge}
 
 # Environment specifics
-export HYPERSHIFT_CLI_INSTALL="true"
-export HYPERSHIFT_CLI_VERSION="master"
-export HYPERSHIFT_CLI_FORK="https://github.com/openshift/hypershift"
+export HYPERSHIFT_CLI_INSTALL=${HYPERSHIFT_CLI_INSTALL:-"true"}
+export HYPERSHIFT_CLI_VERSION=${HYPERSHIFT_CLI_VERSION:-"master"}
+export HYPERSHIFT_CLI_FORK=${HYPERSHIFT_CLI_FORK:-"https://github.com/openshift/hypershift"}
 
 # Indexing stats
 export ENABLE_INDEX=${ENABLE_INDEX:-true}
