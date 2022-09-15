829adc595edae523b01e861c2c6780b5617c3aca
Krishna Harsha Voora <14876995+krishvoor@users.noreply.github.com>
2022-09-15 23:58:51 +0530

    Add pre-flight checks (#463)
    * Add pre-flight checks
    
    This PR adds the following
    
    * Pre-flight checks
    * Clones hypershift to temp director
    * Clean-up kubeadmin configs and other generated files
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    * Updates PATH variable
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    * Update pre_flight_checks()
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    * Check hypershift version
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>

--- workloads/hyper-scale/common.sh
+++ workloads/hyper-scale/common.sh
@@ -7,16 +7,15 @@ prep(){
     if [[ -z $(go version) ]]; then
         curl -L https://go.dev/dl/go1.18.2.linux-amd64.tar.gz -o go1.18.2.linux-amd64.tar.gz
         tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-        export PATH=$PATH:/usr/local/go/bin
+        export PATH=/usr/local/go/bin:$PATH
     fi
     if [[ ${HYPERSHIFT_CLI_INSTALL} != "false" ]]; then
-        echo "Remove current Hypershift CLI directory.."
-        sudo rm -rf hypershift || true
-        sudo rm /usr/local/bin/hypershift || true
-        git clone -q --depth=1 --single-branch --branch ${HYPERSHIFT_CLI_VERSION} ${HYPERSHIFT_CLI_FORK}    
-        pushd hypershift
+        echo "Building Hypershift binaries locally.."
+        export TEMP_DIR=$(mktemp -d)
+        git clone -q --depth=1 --single-branch --branch ${HYPERSHIFT_CLI_VERSION} ${HYPERSHIFT_CLI_FORK} -v $TEMP_DIR/hypershift
+        pushd $TEMP_DIR/hypershift
         make build
-        sudo cp bin/hypershift /usr/local/bin
+        export PATH=$TEMP_DIR/hypershift/bin:$PATH
         popd
     fi
     if [[ -z $(rosa version)  ]]; then
@@ -24,7 +23,7 @@ prep(){
         sudo curl -L $(curl -s https://api.github.com/repos/openshift-online/ocm-cli/releases/latest | jq -r ".assets[] | select(.name == \"ocm-linux-amd64\") | .browser_download_url") --output /usr/local/bin/ocm
         sudo chmod +x /usr/local/bin/rosa && chmod +x /usr/local/bin/ocm
     fi
-    if [[ -z $(oc help) ]]; then
+    if [[ -z $(oc version) ]]; then
         rosa download openshift-client
         tar xzvf openshift-client-linux.tar.gz
         sudo mv oc kubectl /usr/local/bin/
@@ -46,9 +45,24 @@ setup(){
     echo aws_secret_access_key=$AWS_SECRET_ACCESS_KEY >> aws_credentials
     rosa login --env=${ROSA_ENVIRONMENT}
     ocm login --url=https://api.stage.openshift.com --token="${ROSA_TOKEN}"
+    hypershift --version
+    oc version --client
     rosa whoami
     rosa verify quota
     rosa verify permissions
+}
+
+pre_flight_checks(){
+    echo "Pre flight checks started"
+    export MULTI_AZ=$(rosa describe cluster -c $MGMT_CLUSTER_NAME -o json | jq -r [.multi_az] | jq -r .[])
+
+    if [[ "${MULTI_AZ}" == "true" ]]; then
+        echo "Pre flight checks passed"
+    else
+        echo "Pre flight checks failed, cluster should be multi-az enabled"
+        rm -rf $TEMP_DIR || true
+        exit 1
+    fi
 }
 
 install(){
@@ -156,6 +170,11 @@ cleanup(){
     sleep 10
     ROUTE_ID=$(aws route53 list-hosted-zones --output text --query HostedZones | grep $BASEDOMAIN | grep -v terraform | awk '{print$2}' | awk -F/ '{print$3}')
     for id in $ROUTE_ID; do aws route53 delete-hosted-zone --id=$id || true ; done
+    rm -f *-admin-kubeconfig || true
+    rm -f pull-secret || true
+    rm -rf kube-burner.tar.gz|| true
+    rm -f hypershift-metrics.yaml baseconfig.yml || true
+    rm -f aws_credentials || true
 }
 
 index_mgmt_cluster_stat(){
