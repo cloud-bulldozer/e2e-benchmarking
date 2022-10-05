dad1e15367da9e090e8355f3488a396affd9ae64
Raul Sevilla <rsevilla@redhat.com>
2022-10-05 13:15:58 +0200

    Add duration flag
    Fixes: https://github.com/cloud-bulldozer/e2e-benchmarking/issues/480
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- workloads/network-perf/common.sh
+++ workloads/network-perf/common.sh
@@ -11,7 +11,7 @@ export_defaults() {
   network_type=$(oc get network cluster -o jsonpath='{.status.networkType}' | tr '[:upper:]' '[:lower:]')
   export UUID=$(uuidgen)
   export CLUSTER_NAME=$(oc get infrastructure cluster -o jsonpath="{.status.infrastructureName}")
-  export PROM_TOKEN=$(oc sa get-token -n openshift-monitoring prometheus-k8s || oc sa new-token -n openshift-monitoring prometheus-k8s || oc create token -n openshift-monitoring  prometheus-k8s)
+  export PROM_TOKEN=$(oc sa get-token -n openshift-monitoring prometheus-k8s || oc sa new-token -n openshift-monitoring prometheus-k8s || oc create token -n openshift-monitoring prometheus-k8s --duration=6h)
   nodes=($(oc get nodes -l node-role.kubernetes.io/worker,node-role.kubernetes.io/workload!="",node-role.kubernetes.io/infra!="" -o custom-columns=name:{.metadata.name} --no-headers))
   if [[ ${#nodes[@]} -lt 2 ]]; then
     log "At least 2 worker nodes are required"
