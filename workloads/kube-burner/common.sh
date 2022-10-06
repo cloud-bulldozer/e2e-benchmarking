5b6d78ffbdd461dac0f833c920d8b1ca8c06245c
Murali Krishnasamy <mukrishn@redhat.com>
2022-10-06 14:34:36 -0400

    mgmt worker node metrics

--- workloads/kube-burner/common.sh
+++ workloads/kube-burner/common.sh
@@ -43,6 +43,13 @@ if [[ ${HYPERSHIFT} == "true" ]]; then
     export DAG_ID=$(oc version -o json | jq -r '.openshiftVersion')-$(oc get infrastructure cluster -o jsonpath='{.status.infrastructureName}') # setting a dynamic value
     envsubst < ./grafana-agent.yaml | oc apply -f -
   fi
+  echo "Get all management worker nodes.."
+  export Q_TIME=$(date +"%s")
+  export Q_NODES=""
+  for n in $(curl -k --silent --globoff  ${PROM_URL}/api/v1/query?query='sum(kube_node_role{openshift_cluster_name=~"'${MGMT_CLUSTER_NAME}'",role=~"master|infra|workload"})by(node)&time='$(($Q_TIME-300))'' | jq -r '.data.result[].metric.node'); do
+    Q_NODES=${n}"|"${Q_NODES};
+  done
+  export MGMT_WORKER_ONLY_NODES=${Q_NODES}
 fi
 
 collect_pprof() {
