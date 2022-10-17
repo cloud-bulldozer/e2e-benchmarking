4f4abfe3bcc5266f0d6b4ef958930b33ae36295c
Krishna Harsha Voora <14876995+krishvoor@users.noreply.github.com>
2022-10-18 01:55:10 +0530

    Add Additional tags to Hypershift Clusters (#492)
    * Add Additional tags to Hypershift Clusters
    
    This PR adds following tags to the HyperShift clusters
    1) User:${GITHUB_USER}
    2) {MGMT_CLUSTER_NAME}:{HOSTED_CLUSTER_NAME}
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    * Updates the tags
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>

--- workloads/hyper-scale/common.sh
+++ workloads/hyper-scale/common.sh
@@ -92,7 +92,7 @@ create_cluster(){
     if [[ $RELEASE_IMAGE != "" ]]; then
         RELEASE="--release-image=$RELEASE_IMAGE"
     fi
-    hypershift create cluster aws --name $HOSTED_CLUSTER_NAME --node-pool-replicas=$COMPUTE_WORKERS_NUMBER --base-domain $BASEDOMAIN --pull-secret pull-secret --aws-creds aws_credentials --region $AWS_REGION --control-plane-availability-policy $REPLICA_TYPE --network-type $NETWORK_TYPE --instance-type $COMPUTE_WORKERS_TYPE  ${RELEASE} ${CPO_IMAGE_ARG}
+    hypershift create cluster aws --name $HOSTED_CLUSTER_NAME --node-pool-replicas=$COMPUTE_WORKERS_NUMBER --base-domain $BASEDOMAIN --pull-secret pull-secret --aws-creds aws_credentials --region $AWS_REGION --control-plane-availability-policy $REPLICA_TYPE --network-type $NETWORK_TYPE --instance-type $COMPUTE_WORKERS_TYPE  ${RELEASE} ${CPO_IMAGE_ARG} --additional-tags mgmt-cluster:${MGMT_CLUSTER_NAME}
     echo "Wait till hosted cluster got created and in progress.."
     oc wait --for=condition=available=false --timeout=60s hostedcluster -n clusters $HOSTED_CLUSTER_NAME
     oc get hostedcluster -n clusters $HOSTED_CLUSTER_NAME
