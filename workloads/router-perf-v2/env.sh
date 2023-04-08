0560a0a6311d73ee844af4b28fdfa22d64c09dc0
Sai Sindhur Malleni <smalleni@redhat.com>
2023-04-08 13:46:47 -0500

    Fix router large scale scenario
    Currently we check for greater than or equal to 24 nodes
    excluding workload node to execute the large scenario. This
    means that if you have a 24 worker cluster, you will actually end up running
    the small scale scenario since one node is excludded when counting total workers.
    
    However, we still continue to use the workload node to place the backend nginx pods,
    which makes our logic of determining small/large scale senario and labeling the workload node
    counter-intuitive. The nginx backend pods are fighting for cpu time on the same node where mb
    client is firing requests off of.
    
    This patch does the following:
    1. Counts the workload node also when determining number of worker nodes (still excludes infra)
    2. Exlcudes the workload node from backend pod placement using node anti-affinity
    
    This means that while we still will have the same number of backend pods (2000), the workload
    node is ecluded from nginx pod placement, which should make results more deterministic as the
    workload node exclusively hosts the mb client
    
    Signed-off-by: Sai Sindhur Malleni <smalleni@redhat.com>

--- workloads/router-perf-v2/env.sh
+++ workloads/router-perf-v2/env.sh
@@ -7,7 +7,7 @@ export ES_SERVER=${ES_SERVER:-https://search-perfscale-dev-chmf5l4sh66lvxbnadi4b
 export ES_INDEX=${ES_INDEX:-router-test-results}
 
 # Environment setup
-NUM_NODES=$(oc get node -l node-role.kubernetes.io/worker,node-role.kubernetes.io/workload!=,node-role.kubernetes.io/infra!= --no-headers | grep -cw Ready)
+NUM_NODES=$(oc get node -l node-role.kubernetes.io/worker,node-role.kubernetes.io/infra!= --no-headers | grep -cw Ready)
 LARGE_SCALE_THRESHOLD=${LARGE_SCALE_THRESHOLD:-24}
 METADATA_COLLECTION=${METADATA_COLLECTION:-true}
 KUBE_BURNER_RELEASE_URL=${KUBE_BURNER_RELEASE_URL:-https://github.com/cloud-bulldozer/kube-burner/releases/download/v0.16.2/kube-burner-0.16.2-Linux-x86_64.tar.gz}
