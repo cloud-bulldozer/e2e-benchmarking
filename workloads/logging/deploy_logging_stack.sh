7fab821980614458a300dc6a598e4cae59ffaea4
David Sanz Moreno <morenod@users.noreply.github.com>
2023-04-27 16:30:32 +0200

    Update logging workload to CLO 5.6 (#549)

--- workloads/logging/deploy_logging_stack.sh
+++ workloads/logging/deploy_logging_stack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+#set -e
 
 # Source env.sh to read all the vars
 source env.sh
@@ -28,7 +28,7 @@ fi
 
 function install() {
   # create cluster logging and elasticsearch resources
-  if [[ $CUSTOM_ES_URL != "" ]]; then
+  if [[ "${CUSTOM_ES_URL}" != "" ]]; then
     log "Creating cluster logging with custom elasticsearch backend"
     envsubst < ./files/logging-stack_custom_es.yml | oc create -f -
   else
@@ -37,22 +37,15 @@ function install() {
   fi
 }
 
-wait_time=0
 function cleanup() {
-  oc delete --wait=true project openshift-logging --ignore-not-found
-  oc delete --wait=true project openshift-operators-redhat --ignore-not-found
-  while [[ $( oc get projects | grep -w "openshift-logging\|openshift-operators-redhat") ]]; do
-    sleep 5
-    wait_time=$((wait_time+5))
-    if [[ $wait_time -ge $TIMEOUT ]]; then
-      log "openshift-logging/openshift-operators-redhat namespaces still exists after $TIMEOUT, please check. Exiting"
-      exit 1
-    fi
-  done
+  if ! oc delete --wait=true project openshift-logging openshift-operators-redhat --ignore-not-found --timeout=180s; then
+    log "openshift-logging/openshift-operators-redhat namespaces exists after 3 minutes, please check. Exiting..."
+    exit 1
+  fi
 }
 
 # Delete the namespaces if already exists
-log "Deleting openshift-logging/openshift-operators-redhat namespaces if exists"
+log "Deleting openshift-logging/openshift-operators-redhat namespaces if exists (3 minutes wait)"
 cleanup
 
 # Install the necessary objects for setting up elastic and logging operators and create a cluster logging instance
@@ -66,25 +59,15 @@ sleep 60
 oc wait --for=condition=available -n openshift-logging deployment/cluster-logging-operator --timeout=180s
 log "Checking the status"
 for deployment in $( oc get deployments -n openshift-logging | awk 'NR!=1{print $1}'); do oc wait --for=condition=available -n openshift-logging deployment/$deployment --timeout=180s; done
-wait_time=0
-while [[ $( oc get daemonset.apps/fluentd -n openshift-logging -o=jsonpath='{.status.desiredNumberScheduled}' ) != $( oc get daemonset.apps/fluentd -n openshift-logging -o=jsonpath='{.status.numberReady}' ) ]]; do
-  log "Waiting for fluentd daemonset"
-  sleep 5
-  wait_time=$((wait_time+5))
-  if [[ $wait_time -ge $TIMEOUT ]]; then
-    log "Fluentd daemonset is not ready after $TIMEOUT, please check. Exiting"
-    exit 1
-  fi
-done
 log "Logging stack is up"
 
-if [[ $CUSTOM_ES_URL == "" ]]; then
+if [[ -z "${CUSTOM_ES_URL}" ]]; then
   # Expose the elasticsearch service
   log "Exposing the elasticsearch service by creating a route"
   oc extract secret/elasticsearch --to=/tmp/ --keys=admin-ca --confirm -n openshift-logging
   cp files/elasticsearch-route.yml /tmp/elasticsearch-route.yml
   cat /tmp/admin-ca | sed -e "s/^/      /" >> /tmp/elasticsearch-route.yml
   oc create -f /tmp/elasticsearch-route.yml -n openshift-logging
-  routeES=`oc get route elasticsearch -n openshift-logging -o jsonpath={.spec.host}`
+  routeES=$(oc get route elasticsearch -n openshift-logging -o "jsonpath={.spec.host}")
   log "Elasticsearch is exposed at $routeES, bearer token is needed to access it"
 fi
