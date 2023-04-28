9465a1076bcb0c39b6079f30e18f605f814c4256
Joe Talerico (rook) <jtaleric@redhat.com>
2023-04-28 12:01:25 -0400

    Adding k8s-netperf (#559)
    Signed-off-by: Joe Talerico <jtaleric@redhat.com>

---
+++ workloads/network-perf-v2/env.sh
@@ -0,0 +1,12 @@
+# Common
+export ES_SERVER=${ES_SERVER:-https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com:443}
+
+# k8s-netperf version
+export NETPERF_VERSION=${VERSION:-v0.1.6}
+export OS=${OS:-Linux}
+export ARCH=$(arch)
+export NETPERF_URL=${NETPERF_URL:-https://github.com/jtaleric/k8s-netperf/releases/download/${NETPERF_VERSION}/k8s-netperf_${OS}_${NETPERF_VERSION}_${ARCH}.tar.gz}
+
+# Workload
+export WORKLOAD=${WORKLOAD:-smoke.yaml}
+export TEST_TIMEOUT=${TEST_TIMEOUT:-7200}
---
+++ workloads/network-perf-v2/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source ./env.sh
+source ../../utils/common.sh
+
+curl -sS -L $NETPERF_URL | tar -xz
+
+# Assuming kubeconfig is set
+oc create ns netperf
+oc create sa netperf -n netperf
+oc adm policy add-scc-to-user hostnetwork -z netperf -n netperf
+
+log "###############################################"
+log "Workload: ${WORKLOAD}"
+log "###############################################"
+
+timeout $TEST_TIMEOUT ./k8s-netperf --debug --metrics --iperf --all --config ${WORKLOAD} --search $ES_SERVER --clean=true
+run=$?
+
+# Add debugging info (will be captured in each execution output)
+echo "============ Debug Info ============"
+echo k8s-netperf version $NETPERF_VERSION
+oc get pods -n netperf -o wide
+oc get nodes -o wide
+oc get machineset -A
+
+log "Finished workload ${0} ${WORKLOAD}, exit code ($run)"
+
+cat *.csv
+
+exit $run
