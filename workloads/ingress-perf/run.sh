7dc2fccfdf74f198847dc6284e3a47aa108c3869
Raul Sevilla <rsevilla@redhat.com>
2023-05-31 12:01:33 +0200

    Bump ingress-perf version
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- workloads/ingress-perf/run.sh
+++ workloads/ingress-perf/run.sh
@@ -5,7 +5,7 @@ set -e
 ES_SERVER=${ES_SERVER:-https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com}
 ES_INDEX=${ES_INDEX:-ingress-performance}
 LOG_LEVEL=${LOG_LEVEL:-info}
-VERSION=${VERSION:-0.1}
+VERSION=${VERSION:-0.2.1}
 CONFIG=${CONFIG:?}
 BASELINE_UUID=${BASELINE_UUID:-}
 BASELINE_INDEX=${BASELINE_INDEX:-ingress-performance-baseline}
@@ -17,9 +17,12 @@ download_binary(){
 }
 
 download_binary
-cmd="./ingress-perf run --cfg ${CONFIG} --es-server=${ES_SERVER} --es-index=${ES_INDEX} --loglevel=${LOG_LEVEL}"
+cmd="./ingress-perf run --cfg ${CONFIG} --loglevel=${LOG_LEVEL}"
+if [[ -n ${ES_SERVER} ]]; then
+  cmd+=" --es-server=${ES_SERVER} --es-index=${ES_INDEX}"
+fi
 if [[ -n ${BASELINE_UUID} ]]; then
-  cmd+=" --baseline--uuid=${BASELINE_UUID} --baseline-index=${BASELINE_INDEX} --tolerancy=${TOLERANCY}"
+  cmd+=" --baseline-uuid=${BASELINE_UUID} --baseline-index=${BASELINE_INDEX} --tolerancy=${TOLERANCY}"
 fi
 echo $cmd
 exec $cmd
