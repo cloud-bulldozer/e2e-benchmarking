b4cd05083a08eb9ff4339dfe522f59f0c2f8f2a3
Joe Talerico (rook) <jtaleric@redhat.com>
2023-06-08 16:27:16 -0400

    k8s-netperf bump to 1.9 (#574)
    Signed-off-by: Joe Talerico <jtaleric@redhat.com>

--- workloads/network-perf-v2/env.sh
+++ workloads/network-perf-v2/env.sh
@@ -2,7 +2,7 @@
 export ES_SERVER=${ES_SERVER:-https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com:443}
 
 # k8s-netperf version
-export NETPERF_VERSION=${VERSION:-v0.1.8}
+export NETPERF_VERSION=${VERSION:-v0.1.9}
 export OS=${OS:-Linux}
 export ARCH=$(arch)
 export NETPERF_URL=${NETPERF_URL:-https://github.com/cloud-bulldozer/k8s-netperf/releases/download/${NETPERF_VERSION}/k8s-netperf_${OS}_${NETPERF_VERSION}_${ARCH}.tar.gz}
