46bd77f134b63e01db4f208ee0b26bfbd0a84f45
Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.remote.csb>
2024-02-15 10:31:33 -0500

    testing e2e PR on PROW

--- workloads/kube-burner-ocp-wrapper/run.sh
+++ workloads/kube-burner-ocp-wrapper/run.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo "testing from e2e PR"
 ES_SERVER=${ES_SERVER=https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com}
 LOG_LEVEL=${LOG_LEVEL:-info}
 if [ "$KUBE_BURNER_VERSION" = "default" ]; then
