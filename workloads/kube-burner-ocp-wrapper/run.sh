908fb685dca2dccced2396f884ac2d150b9e7522
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-09-13 22:41:03 -0600

    log less

--- workloads/kube-burner-ocp-wrapper/run.sh
+++ workloads/kube-burner-ocp-wrapper/run.sh
@@ -175,6 +175,7 @@ set +e
 #sed -i '' -e 's/\[{{.METRICS}}\]/\[stackrox.yml,{{.METRICS}}\]/' *.yml
 #grep METRICS *.yml
 export CLUSTER_PROM_URL="https://$(oc -n openshift-monitoring get routes prometheus-k8s --no-headers | awk '{print $2}')"
+set +x
 if [[ -z "${TOKEN}" ]]; then
   echo "TOKEN not set"
 fi
@@ -186,6 +187,7 @@ $cmd
 exit_code=$?
 
 $cmd <(echo -e "- endpoint: $(oc -n openshift-monitoring get routes prometheus-k8s --no-headers|awk "{print \$2}")\n  token: $(oc create token -n openshift-monitoring prometheus-k8s)\n  metrics: [{{.EXTRA_METRICS_FILE}}]\n  indexer:\n    esServers: ["{{.ES_SERVER}}"]\n    defaultIndex: {{.ES_INDEX}}')
+set -x
 
 JOB_END=${JOB_END:-$(date -u +"%Y-%m-%dT%H:%M:%SZ")};
 if [ $exit_code -eq 0 ]; then
