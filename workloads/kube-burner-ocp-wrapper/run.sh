bb66db039e2d6dd47aadd4778015fab6762b0adb
vishnuchalla <vchalla@redhat.com>
2024-10-22 14:48:31 -0400

    Update run.sh handling nil conditions

--- workloads/kube-burner-ocp-wrapper/run.sh
+++ workloads/kube-burner-ocp-wrapper/run.sh
@@ -122,11 +122,13 @@ EOF
 
 download_binary
 if [[ ${WORKLOAD} =~ "index" || ${WORKLOAD} =~ "workers-scale" ]]; then
-  cmd="${KUBE_DIR}/kube-burner-ocp ${WORKLOAD} --uuid=${UUID} --start=$START_TIME --end=$((END_TIME+600)) --log-level ${LOG_LEVEL}"
-  if [[ "$START_TIME" != "0" && "$START_TIME" != 0 && "$END_TIME" != "0" && "$END_TIME" != 0 ]]; then
+  
+  if [[ "$START_TIME" != 0 && "$END_TIME" != 0 ]]; then
     JOB_START=$(date -u -d "@$START_TIME" +"%Y-%m-%dT%H:%M:%SZ")
-    JOB_END=$(date -u -d "@$((END_TIME + 600))" +"%Y-%m-%dT%H:%M:%SZ")
+    END_TIME=$(expr $END_TIME + 600)
+    JOB_END=$(date -u -d "@$END_TIME" +"%Y-%m-%dT%H:%M:%SZ")
   fi
+  cmd="${KUBE_DIR}/kube-burner-ocp ${WORKLOAD} --uuid=${UUID} --start=$START_TIME --end=$END_TIME --log-level ${LOG_LEVEL}" --gc=${GC}
 else
   cmd="${KUBE_DIR}/kube-burner-ocp ${WORKLOAD} --log-level=${LOG_LEVEL} --qps=${QPS} --burst=${BURST} --gc=${GC} --uuid ${UUID}"
 fi
