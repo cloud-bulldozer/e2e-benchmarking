c3be8cbd0725e9f2b7d02731ad0f5d78f8fa9de0
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2025-04-14 15:17:25 +0530

    udnl2 and l3 changes

--- workloads/network-perf-v2/run.sh
+++ workloads/network-perf-v2/run.sh
@@ -61,7 +61,8 @@ add_flag "search" "${ES_SERVER}"
 add_flag "tcp-tolerance" "${TOLERANCE}"
 add_flag "uuid" "${UUID}"
 add_flag "vm" "${VM}"
-add_flag "udn" "${UDN}"
+add_flag "udnl2" "${UDNL2}"
+add_flag "udnl3" "${UDNL3}"
 
 # Execute the constructed command
 eval "$cmd"
